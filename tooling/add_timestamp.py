46b5187d3cc6f3231b465297cba87a24e4cc45d2
Dany.A <arabodanny002@gmail.com>
2026-01-08 21:47:39 -0500

    Delete tooling directory

--- tooling/add_timestamp.py
+++
@@ -1,6 +0,0 @@
-# tooling/add_timestamp.py
-from datetime import datetime
-
-index_file = "web/index.html"
-with open(index_file, "a") as f:
-    f.write(f"\n<!-- Last updated: {datetime.now()} -->\n")
