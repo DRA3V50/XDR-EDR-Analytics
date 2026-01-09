88edd67de164f3ee8836d1ca81a38055a2c873ed
Dany.A <arabodanny002@gmail.com>
2026-01-08 20:53:44 -0500

    Update telemetry_parser.cs

--- tooling/telemetry_parser.cs
+++ tooling/telemetry_parser.cs
@@ -1,1 +1,18 @@
+using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
+class TelemetryParser
+{
+    static void Main()
+    {
+        string jsonFile = "sample_telemetry.json";
+        var jsonData = File.ReadAllText(jsonFile);
+        var jObject = JObject.Parse(jsonData);
+
+        foreach (var proc in jObject["DeviceProcessEvents"])
+        {
+            Console.WriteLine($"Process: {proc["ProcessName"]}, CommandLine: {proc["ProcessCommandLine"]}");
+        }
+    }
+}
